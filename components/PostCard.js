d7b77deeba37a4bc1e369f191d97e0f39f511249
irfan-za <fanzahran123@gmail.com>
2021-12-15 13:02:24 +0700

    setup code

---
+++ components/PostCard.js
@@ -0,0 +1,9 @@
+export default function PostCard({post}) {
+  return (
+    <div>
+      {post.title}
+      {post.excerpt}
+    </div> 
+  )
+}
+
---
+++ components/PostWidget.js
@@ -0,0 +1,9 @@
+export default function PostWidget() {
+  return (
+    <div>
+      Widget
+    </div>
+  )
+}
+
+
