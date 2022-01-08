749dc5695c41b6981bea8d0683cf8e411eaa1bce
irfan-za <fanzahran123@gmail.com>
2022-01-08 10:05:29 +0700

    create author component

--- components/Author.js
+++ components/Author.js
@@ -1,9 +1,13 @@
+import Image from "next/image"
 
-
-function Author() {
+function Author({author}) {
   return (
-    <div>
-      Author
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image unoptimized src={author.photo.url} alt={Author.name} width="100px" height="100px" className="align-middle rounded-full" />
+        <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+        <p className="text-white text-lg">{author.bio}</p>
+      </div>
     </div>
   )
 }
