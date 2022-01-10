431a6e77622458c33596002e2f5da466d28bce56
irfan-za <fanzahran123@gmail.com>
2022-01-10 19:14:37 +0700

    add svg file

--- components/PostDetail.js
+++ components/PostDetail.js
@@ -54,7 +54,9 @@ function PostDetail({post}) {
             <p className="inline align-middle text-gray-700 ml-2 text-lg">{post.author.name}</p>
           </div>
           <div className="font-medium text-gray-700">
-            date SVG
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
             <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
           </div>
         </div>
