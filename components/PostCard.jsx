a084ea6ca11017d5a71a1dd8b9570b57f8374387
irfan-za <fanzahran123@gmail.com>
2022-01-27 10:40:41 +0700

    update postcard

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -28,7 +28,7 @@ const PostCard = ({ post }) => (
           className="align-middle rounded-full"
           src={post.author.photo.url}
         />
-        <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
+        <p className="inline align-middle text-gray-700 mb-0 ml-2 font-medium text-lg">{post.author.name}</p>
       </div>
       <div className="font-medium text-gray-700">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
