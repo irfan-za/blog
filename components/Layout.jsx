a788d9a6d0ec841cb50b46e457603010be8c2301
irfan-za <fanzahran123@gmail.com>
2022-02-24 18:45:40 +0700

    update meta img

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -14,7 +14,7 @@ export default function Layout(props) {
         <meta property="og:title" content={props.title} />
         <meta property="og:description" content={props.desc} />
         <meta property="og:url" content="https://z-blog.vercel.app" />
-        <meta property="og:image" content={props.img} />
+        <meta property="og:image:url" content={props.img} />
       </Head>
       <Header />
       {children}
