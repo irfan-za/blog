e8e48deff5693143d2658dd64894312ead191c4e
irfan-za <fanzahran123@gmail.com>
2022-02-25 16:31:55 +0700

    update fontsize

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -89,7 +89,7 @@ const PostDetail = ({ post }) => {
               <span className="align-middle text-sm">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
             </div>
           </div>
-          <h1 className="mb-8 text-2xl lg:text-3xl font-semibold">{post.title}</h1>
+          <h1 className="mb-8 text-lg md:text-2xl lg:text-3xl  font-semibold">{post.title}</h1>
           {post.content.raw.children.map((typeObj, index) => {
             const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
 
