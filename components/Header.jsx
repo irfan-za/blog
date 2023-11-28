12579cd1abf05fb4c20c49d90f90f76c0bde3c3a
Muhammad Irfan Zahran <fanzahran123@gmail.com>
2023-11-28 18:17:25 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
       <div className="border-b w-full inline-block border-white py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Blog</span>
+            <h1 className="cursor-pointer font-bold text-4xl text-white">Z Blog</h1>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
